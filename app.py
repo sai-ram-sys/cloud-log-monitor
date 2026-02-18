d4ed4b422ddaec30bf2d65aa529452dbad410430
sai-ram-sys <sai.ramnair05@gmail.com>
2026-02-18 14:38:46 +0530

    test commit

--- app.py
+++ app.py
@@ -2,19 +2,42 @@ from flask import Flask
 import logging
 import os
 import random
+import smtplib
+from email.message import EmailMessage
 
 app = Flask(__name__)
 
 logging.basicConfig(filename='app.log', level=logging.INFO)
 
+EMAIL = os.environ.get("EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
+
+def send_email():
+    try:
+        msg = EmailMessage()
+        msg.set_content("Error detected in cloud application log!")
+        msg["Subject"] = "Cloud Alert: Error Found"
+        msg["From"] = EMAIL
+        msg["To"] = EMAIL
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(EMAIL, PASSWORD)
+            server.send_message(msg)
+
+        print("Email sent successfully!")
+
+    except Exception as e:
+        print("Email failed:", e)
+
 @app.route('/')
 def home():
     if random.randint(1,5) == 3:
         logging.error("ERROR: Something went wrong!")
-        return "Error occurred and logged!"
+        send_email()
+        return "Error occurred and email sent!"
     else:
         logging.info("INFO: Normal request")
-        return "App is running normally."
+        return "App running normally."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
